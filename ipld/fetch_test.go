29e7189805820213e0e43478ceb5566f09124743
Hlib <hlibwondertan@gmail.com>
2020-04-20 14:14:58 +0800

    Add test for FetchAbsent

--- ipld/fetch_test.go
+++ ipld/fetch_test.go
@@ -17,7 +17,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestFetch(t *testing.T) {
+func TestFetchDAG(t *testing.T) {
 	const (
 		nsize = 512
 		rsize = nsize * 256
@@ -33,13 +33,74 @@ func TestFetch(t *testing.T) {
 	err := FetchDAG(ctx, nd.Cid(), &offlineStreamer{getter: fillstore.Get}, &fakeAdder{emptystore})
 	require.Nil(t, err, err)
 
-	ids, err := fillstore.AllKeysChan(ctx)
+	assertEqualBlockstore(t, ctx, fillstore, emptystore)
+	assertEqualBlockstore(t, ctx, emptystore, fillstore)
+}
+
+func TestFetchAbsent(t *testing.T) {
+	const (
+		nsize = 512
+		rsize = nsize * 256
+	)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	fillstore := blockstore.NewBlockstore(sync.MutexWrap(datastore.NewMapDatastore()))
+	halfstore := blockstore.NewBlockstore(sync.MutexWrap(datastore.NewMapDatastore()))
+
+	nd1 := dagFromReader(t, io.LimitReader(rand.Reader, rsize), &fakeAdder{fillstore}, nsize)
+	copyBlockstore(t, ctx, fillstore, halfstore)
+	nd2 := dagFromReader(t, io.LimitReader(rand.Reader, rsize), &fakeAdder{fillstore}, nsize)
+	nd3 := dagFromReader(t, io.LimitReader(rand.Reader, rsize), &fakeAdder{fillstore}, nsize)
+
+	root := merkledag.NodeWithData(nil)
+	root.AddNodeLink("1", nd1)
+	root.AddNodeLink("2", nd2)
+	root.AddNodeLink("3", nd3)
+	fillstore.Put(root)
+
+	s := &offlineStreamer{getter: fillstore.Get}
+	err := FetchAbsent(ctx, root.Cid(), s, halfstore)
+	require.Nil(t, err, err)
+
+	assertEqualBlockstore(t, ctx, fillstore, halfstore)
+	assertEqualBlockstore(t, ctx, halfstore, fillstore)
+
+	for _, id := range s.streamed {
+		if id.Equals(nd1.Cid()) {
+			t.Fatal("streamed existed blocks")
+		}
+	}
+}
+
+func assertEqualBlockstore(t *testing.T, ctx context.Context, exp, given blockstore.Blockstore) {
+	ids, err := exp.AllKeysChan(ctx)
 	require.Nil(t, err, err)
 
 	for id := range ids {
-		ok, err := emptystore.Has(id)
+		ok, err := given.Has(id)
 		require.Nil(t, err, err)
 		assert.True(t, ok, "store does not have required block")
+	}
+}
+
+func copyBlockstore(t *testing.T, ctx context.Context, from, to blockstore.Blockstore) {
+	ch, err := from.AllKeysChan(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for id := range ch {
+		b, err := from.Get(id)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = to.Put(b)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 }
 
@@ -88,7 +149,8 @@ func (f *fakeAdder) AddMany(_ context.Context, nds []format.Node) error {
 }
 
 type offlineStreamer struct {
-	getter func(cid.Cid) (blocks.Block, error)
+	getter   func(cid.Cid) (blocks.Block, error)
+	streamed []cid.Cid
 }
 
 func (f *offlineStreamer) Stream(ctx context.Context, ids <-chan []cid.Cid) <-chan blocks.Block {
@@ -107,6 +169,7 @@ func (f *offlineStreamer) Stream(ctx context.Context, ids <-chan []cid.Cid) <-ch
 					if err != nil {
 						return
 					}
+					f.streamed = append(f.streamed, id)
 
 					select {
 					case out <- b:
