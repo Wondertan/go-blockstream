1796756db6c275a4740d87b2ab6b741136b2b95c
Wondertan <hlibwondertan@gmail.com>
2020-12-02 03:17:17 +0200

    check for a result error in explore

--- ipld/fetch.go
+++ ipld/fetch.go
@@ -3,7 +3,7 @@ package ipld
 import (
 	"context"
 
-	"github.com/Wondertan/go-blockstream/block"
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	format "github.com/ipfs/go-ipld-format"
 
@@ -12,8 +12,8 @@ import (
 
 // Traverse traverses and fetches whole IPLD graph from the stream.
 func Traverse(ctx context.Context, id cid.Cid, ses blockstream.Streamer) error {
-	return blockstream.Explore(ctx, id, ses, func(res block.Result) ([]cid.Cid, error) {
-		nd, err := format.Decode(res)
+	return blockstream.Explore(ctx, id, ses, func(b blocks.Block) ([]cid.Cid, error) {
+		nd, err := format.Decode(b)
 		if err != nil {
 			return nil, err
 		}
